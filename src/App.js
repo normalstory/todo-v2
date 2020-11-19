dd884b6e047b108497d87823fb0c3300e9a7e45f
thinknormal <nornalstory@gmail.com>
2020-11-19 11:25:53 +0900

    todolist-react-cra v2 -start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TodoTemplate from "./components/TodoTemplate";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TodoTemplate>todo 앱을 만들자 </TodoTemplate>
     </div>
   );
-}
+};
 
 export default App;
