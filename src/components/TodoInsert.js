d55371d7fe730093f22fd3aedfb265957f71fdf5
thinknormal <nornalstory@gmail.com>
2020-11-19 13:30:02 +0900

    insert 컴포넌트에 onInsert기능 적용

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -2,12 +2,20 @@ import React, { useState, useCallback } from "react";
 import { MdAdd } from "react-icons/md"; //https://react-icons.github.io/
 import "./TodoInsert.scss";
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
   const [value, setValue] = useState("");
   const onChange = useCallback((e) => setValue(e.target.value), []);
 
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue("");
+      e.preventDefault(); //submit 예약어의 본래 디폴트기능(새로고침) 정지
+    },
+    [onInsert, value]
+  );
   return (
-    <form className="TodoInsert">
+    <form className="TodoInsert" onSubmit={onSubmit}>
       <input
         placeholder="할일을 입력하세요"
         value={value}
