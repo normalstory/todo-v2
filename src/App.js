fdff9d65631d7ab27769713054d324807944c447
thinknormal <nornalstory@gmail.com>
2020-11-19 11:46:38 +0900

    add - insert

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import TodoTemplate from "./components/TodoTemplate";
+import TodoInsert from "./components/TodoInsert";
 
 const App = () => {
   return (
     <div>
-      <TodoTemplate>todo 앱을 만들자 </TodoTemplate>
+      <TodoTemplate>
+        <TodoInsert />
+      </TodoTemplate>
     </div>
   );
 };
