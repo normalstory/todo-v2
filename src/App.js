d95c9b50880ec0b02e2fae93e040769289ed6b4d
thinknormal <nornalstory@gmail.com>
2020-11-19 12:50:56 +0900

    useState활용 더미 리스트 구현하기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "react basic study", checked: true },
+    { id: 2, text: "component styling study", checked: false },
+    { id: 3, text: "building app study", checked: false },
+  ]);
+
   return (
     <div>
       <TodoTemplate>
         <TodoInsert />
-        <TodoList />
+        <TodoList todos={todos} />
       </TodoTemplate>
     </div>
   );
