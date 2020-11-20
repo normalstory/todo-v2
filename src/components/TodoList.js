72e11a33393f603dbd44f8233e8d7064cfb3a208
thinknormal <nornalstory@gmail.com>
2020-11-20 11:30:16 +0900

    //virtualized 최적화

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { List } from "react-virtualized";
 import TodoListItem from "./TodoListItem";
 import "./TodoList.scss";
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
+  const rowRenderer = useCallback(
+    //virtualized 최적화
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
+        <TodoListItem
+          todo={todo}
+          key={key}
+          onRemove={onRemove}
+          onToggle={onToggle}
+          style={style}
+        />
+      );
+    },
+    [onRemove, onToggle, todos]
+  );
+
   return (
-    <div className="TodoList">
+    // <div className="TodoList">
+    //   {todos.map((todo) => (
+    //     <TodoListItem
+    //       todo={todo}
+    //       key={todo.id}
+    //       onRemove={onRemove}
+    //       onToggle={onToggle}
+    //     />
+    //   ))}
+    // </div>
+    <List //virtualized 최적화
+      className="TodoList"
+      width={512}
+      height={513}
+      rowCount={todos.length}
+      rowHeight={57}
+      rowRenderer={rowRenderer}
+      list={todos}
+      style={{ outline: "none" }}
+    >
       {todos.map((todo) => (
         <TodoListItem
           todo={todo}
@@ -13,8 +50,8 @@ const TodoList = ({ todos, onRemove, onToggle }) => {
           onToggle={onToggle}
         />
       ))}
-    </div>
+    </List>
   );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
